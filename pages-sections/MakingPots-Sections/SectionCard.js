0ee224102792624ed92af53f9d044adfedfdd012
Gabby Lampner <gabbylampner@dhcp-10-142-246-246.uawifi.arizona.edu>
2021-10-28 16:13:26 -0700

    making pots updates

---
+++ pages-sections/MakingPots-Sections/SectionCard.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import { CardActionArea } from '@material-ui/core';
+import classNames from 'classnames';
+import styles from "styles/jss/nextjs-material-kit/pages/makingPots.js";
+import {makeStyles} from "@material-ui/core/styles"
+import Link from "next/link";
+
+const useStyles = makeStyles(styles);
+
+export default function ActionAreaCard({card}) {
+    const classes = useStyles();
+    return (
+        <div>
+            {/* dont know what sx={maxWidth: 345} does*/}
+            
+                <a href={card.href}>
+            <Card className={classes.card}>
+            
+              <CardActionArea> 
+                
+                <CardMedia component="img" image={card.img} className={classes.cardImage}/>
+                    <CardContent> 
+                        <Typography gutterBottom variant="h5" className={classes.cardTitle} >
+                           {card.desc}
+                        </Typography>
+                    </CardContent>
+                
+               </CardActionArea>
+               
+            </Card>
+            </a>
+            
+        </div>
+    );
+  }
--- pages/makingPots.js
+++ pages/makingPots.js
@@ -30,7 +30,9 @@ import DialogTitle from "@material-ui/core/DialogTitle";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogActions from "@material-ui/core/DialogActions";
 
-import styles from "styles/jss/nextjs-material-kit/pages/wallOfPots.js";
+import Grid from "@material-ui/core/Grid";
+
+import styles from "styles/jss/nextjs-material-kit/pages/makingPots.js";
 
 // Sections for this page
 import Introduction   from "pages-sections/MakingPots-Sections/Introduction.js";
@@ -39,9 +41,10 @@ import PaddleAndAvil  from "pages-sections/MakingPots-Sections/PaddleAndAvil.js"
 import CoilAndScrape  from "pages-sections/MakingPots-Sections/CoilAndScrape.js";
 import MakingIntro from "pages-sections/MakingPots-Sections/MakingIntro.js";
 import Sequence from "pages-sections/MakingPots-Sections/Sequence.js";
-//import SectionCard from "pages-sections/MakingPots-Sections/SectionCard.js";
+import SectionCard from "pages-sections/MakingPots-Sections/SectionCard.js";
 
 const useStyles = makeStyles(styles);
+
 
 export default function MakingPots(props) {
   const [classicModal, setClassicModal] = React.useState(false);
@@ -53,6 +56,31 @@ export default function MakingPots(props) {
     classes.imgFluid
   );
   const navImageClasses = classNames(classes.imgRounded, classes.imgGallery);
+  
+  const cards = [
+    {
+        img:"/img/2014-41-073_scan1.jpg",
+        desc:"Paddle and Anvil",
+        href: "../pages-sections/MakingPots-Sections/PaddleAndAvil.js"
+        
+    },
+    {
+        img:"img/c-27562_scan01.jpg",
+        desc:"Coil and Scrape",
+        href: "../pages-sections/MakingPots-Sections/CoilAndScrape.js"
+    },
+    {
+        img:"img/c-27676_scan01.jpg",
+        desc:"Pottery Decoration",
+        href: "../pages-sections/MakingPots-Sections/Decoration.js"
+    },
+    {
+        img:"img/c-28199_scan1.jpg",
+        desc:"Making Pottery",
+        href: "../pages-sections/MakingPots-Sections/MakingIntro.js"
+    }
+];
+  
   return (
     <div>
       <Header
@@ -66,6 +94,7 @@ export default function MakingPots(props) {
         }}
         {...rest}
       />
+
       <Parallax small filter responsive image="/img/exhibit.jpeg">
         <div className={classes.container}>
           <GridContainer>
@@ -77,8 +106,20 @@ export default function MakingPots(props) {
         </div>
       </Parallax>
       <div className={classNames(classes.main, classes.mainRaised)}>
+
+      <Introduction />     
+
+      <Grid container spacing={4} justify="center">
+        {cards.map(card => (
+          <Grid item key={card}>
+            <SectionCard card={card}/>
+          </Grid>
+        ))}
+      </Grid>
+      
+            
             <GridContainer justify="center"> 
-              <Introduction />
+              
               <PaddleAndAvil />
               <CoilAndScrape />
               <Decoration />
